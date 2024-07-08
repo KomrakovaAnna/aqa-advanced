20ac727913137b4192e8599a556994d85f16b15c
Anna Komrakova <komrakova.anna@gmail.com>
2024-07-08 19:02:51 +0100

    hw5

---
+++ home-work-5/task3.js
@@ -0,0 +1,12 @@
+const number = 1;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+console.log(' * * * * * * *');
+const number1 = 10;
+let j = 1;
+while (j <= 10) {
+  console.log(` ${number1} x ${j} = ${number1 * j}`);
+  j++;
+}
