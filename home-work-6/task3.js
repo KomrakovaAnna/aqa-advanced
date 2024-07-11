632572b74aef2e7adfdcba4cf5f0735c51204327
Anna Komrakova <komrakova.anna@gmail.com>
2024-07-11 18:07:59 +0100

    hw6

---
+++ home-work-6/task3.js
@@ -0,0 +1,13 @@
+function checkOrder(available, ordered) {
+  if (ordered === 0) {
+    console.log('Your order is empty');
+  } else if (available >= ordered) {
+    console.log('Your order is accepted');
+  } else {
+    console.log('Your order is too large, we don’t have enough goods.');
+  }
+}
+
+checkOrder(10, 0);
+checkOrder(3, 10);
+checkOrder(10, 3);
