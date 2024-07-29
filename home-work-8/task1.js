df39c7d786f14ecec20292db87e909d94c43e9c6
Anna Komrakova <komrakova.anna@gmail.com>
2024-07-29 17:48:18 +0100

    hw8

---
+++ home-work-8/task1.js
@@ -0,0 +1,23 @@
+const numbers = [2, -5, 0, 7, -3, 0, 10, -8];
+
+let positiveCount = 0;
+
+let negativeCount = 0;
+
+let zeroCount = 0;
+
+for (let number of numbers) {
+  if (number == 0) {
+    zeroCount += 1;
+  } else if (number > 0) {
+    positiveCount += 1;
+  } else if (number < 0) {
+    negativeCount += 1;
+  } else {
+    console.log('Not valid array');
+  }
+}
+
+console.log(`Кількість позитивних чисел: ${positiveCount}`);
+console.log(`Кількість негативних чисел: ${negativeCount}`);
+console.log(`Кількість нульових чисел: ${zeroCount}`);
---
+++ home-work-8/task6.js
@@ -0,0 +1,5 @@
+const numbersList = [1, 10, 14, 2, 4, 5, 43, 34];
+const ascendingReleaseDates = numbersList.toSorted((a, b) => a - b);
+
+console.log(numbersList);
+console.log(ascendingReleaseDates);
