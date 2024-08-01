d490dae6d5d229923cc8eab78d927a8f18020e0b
Anna Komrakova <komrakova.anna@gmail.com>
2024-08-01 23:46:06 +0100

    hw9

---
+++ home-work-9/task1.js
@@ -0,0 +1,12 @@
+let student = {};
+
+student.firstName = 'Ріта';
+student.lastName = 'Комракова';
+student.age = 21;
+student.courses = ['Математика', 'Історія', 'Програмування'];
+
+console.log(student);
+
+console.log(
+  `Name: ${student.firstName}, Surname: ${student.lastName}, Age: ${student.age}, Courses: ${student.courses}`
+);
---
+++ home-work-9/task4.js
@@ -0,0 +1,11 @@
+const person = {
+  firstName: 'Anna',
+  lastName: 'Komrakova',
+  age: 35,
+};
+
+person.email = 'komrakova.anna@gmail.com';
+
+delete person.age;
+
+console.log(person);
